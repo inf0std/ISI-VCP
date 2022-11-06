b3bbbfde6697044335ef77e9d28223a9cfcccb77
[Faycel ahcene] <ff_ahcene@esi.dz>
2022-11-06 20:17:26 +0100

    first ui skeleton

---
+++ src/view/component/conversationTab/conversationTab.js
@@ -0,0 +1,10 @@
+class ConversationTab extends React.Component {
+    constructor(props){
+      super(props)
+    }
+    render() {
+      return (
+        <div>ECRIRE VOTRE CODE ICI</div>
+      );
+    }
+  }
---
+++ src/view/component/userProfile/userProfile.js
@@ -0,0 +1,10 @@
+class UserProfile extends React.Component {
+    constructor(props){
+      super(props)
+    }
+    render() {
+      return (
+        <div>ECRIRE VOTRE CODE ICI</div>
+      );
+    }
+  }

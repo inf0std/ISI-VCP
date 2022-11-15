98a8815ec0bdda624c7845c0dbad304bcbe11832
[Faycel ahcene] <ff_ahcene@esi.dz>
2022-11-15 21:11:01 +0100

    merged ui branch with develop

--- src/server/middleWares/apis/messagery.js
+++ src/server/middleWares/apis/messagery.js
@@ -1,35 +1,4 @@
-const { useId } = require('react');
-const { useImperativeHandle } = require('react');
-const socketIo = require('socketio')
 
-
-module.exports = (server, config) =>{
-
-    var io = socketIo.listen(server);
-
-    io.on('connection', (socket)=>{
-
-        socket.on('', ()=>{
-
-        })
-        //user userId joines conversation convId
-        socket.on('joinConv', data=>{
-            var { useId, convId } = data; 
-            //traitment
-        })
-
-        socket.on('joinConf', data=>{
-            var { userId, confId} = data;
-        })
-
-        //rejoindre une reunion, conversation, conf, debat
-        socket.on('join', data =>{
-
-        })
-
-        //quitter une reunion, conversation, conf, debat
-        socket.on('leave', data =>{
-            
-        })
-    });
+module.exports = roomMaker = (io, config) =>{
+    
 }

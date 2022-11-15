205e62c6b058c712d3c34db16e6e870eb487ac98
[Faycel ahcene] <ff_ahcene@esi.dz>
2022-11-15 14:16:09 +0100

    added queries file

---
+++ src/db/queries.js
@@ -0,0 +1,29 @@
+// this file contains the busniss process queries
+
+getUserConversation = (userId) => {
+    
+}
+
+getUserUnseenMessages = (userId) => {
+
+}
+
+getUserProgrammedEvents = (userId) => { 
+
+}
+
+getUserContacts = (userId) => { 
+
+}
+
+getUserNotifications = (userId) => { 
+    
+}
+
+getUserOrganizations = (userId) => {
+
+}
+
+getUserMissedEvents = (userId) => {
+    
+}

d8ff14aa21d8356d175fa9c67635c4d132fc70bd
djouher <djouher.boukaouma@fgei.ummto.dz>
2022-11-21 15:32:23 +0100

    changed the src to src/view

--- src/server/server.js
+++ src/server/server.js
@@ -1,5 +1,5 @@
 const express = require("express")
-const http = require("hhtp")
+const http = require("http")
 const app = express()
 const server = http.createServer(app)
 const socket = require("socket.io")
--- src/view/scripts/videoCallClient.js
+++ src/view/scripts/videoCallClient.js
@@ -1,18 +1,4 @@
-const express = require("express")
-const http = require("hhtp")
-const app = express()
-const server = http.createServer(app)
-const socket = require("socket.io")
-const io = socket(server)
 
-
-
-
-
-
-
-
-server.listen(3000,() => console.log("hello you"))
 
 
 

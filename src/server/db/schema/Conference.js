9fbcfdedc0d94010375cdc36c4ee7e54996127dd
amira <amira.sellah@fgei.ummto.dz>
2022-11-25 12:11:49 +0100

    Update Conference.js

--- src/server/db/schema/Conference.js
+++ src/server/db/schema/Conference.js
@@ -1,13 +1,38 @@
-const { default: mongoose } = require('mongoose');
-const validator = require('validator');
+const mongoose = require('mongoose')
 
-const Schema = mongoose.Schema;
+const conferSchema = new mongoose.Schema({
+    topic: {
+        type: String,
+        required: true
+    },
+    users: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+        trim: true,
+        joined: { type: Boolean, default: false },
+    }],
+    organisedBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    videocall: [{
+        users: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User',
+            trim: true,
+        },
+        joinedAt: {
+            type: Date,
+        },
 
+    }],
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+}, { timestamps: true })
 
-const ConferenceSchema = new Schema({
-   
-});
-
-
-
-module.exports = mongoose.model('Conference',ConferenceSchema);
+const Confer = mongoose.model('Confer', conferSchema)
+module.exports = { Confer, conferSchema };

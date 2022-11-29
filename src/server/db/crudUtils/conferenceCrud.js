1cd7815858f9b05c84a3eec65c80ba9f223af973
amira <amira.sellah@fgei.ummto.dz>
2022-11-29 20:07:35 +0100

    Update conferenceCrud.js

--- src/server/db/crudUtils/conferenceCrud.js
+++ src/server/db/crudUtils/conferenceCrud.js
@@ -5,130 +5,130 @@ var MongoClient = require('mongodb').MongoClient;
 
 
 // Create and Save a new conference
-exports.create = (req, res) => {
+exports.createConversation = (topic, organisedBy, users, createdAt) => {
     // Validate request
-    if (!req.body.topic) {
-        res.status(400).send({ message: "Content can not be empty!" });
+    if (!topic) {
+        console.log({ message: "Content can not be empty!" });
         return;
     }
 
     // Create a Conference
     const conference = new Conference({
-        topic: req.body.topic,
-        organisedBy: req.body.organisedBy, // get the user id from authentication not seted yet
-        users: req.body.users,
-        videocall: req.body.videocall, // get the user id not seted yet
-        createdAt: req.body.CreatedAt
+        topic: topic,
+        organisedBy: organisedBy, // get the user id from authentication not seted yet
+        users: users,
+        //videocall: videocall, // get the user id not seted yet
+        createdAt: createdAt
     });
 
     // Save conference in the database
     conference
         .save(conference)
-        .then(data => {
-            res.send(data);
-        })
-        .catch(err => {
-            res.status(500).send({
-                message: err.message || "Some error occurred while creating the conference."
-            });
-        });
-};
+    return console.log("success", conference)
 
+    .catch(err => {
+        return console.log({
+            message: err.message || "Some error occurred while creating the conference."
+        });
+    });
+};
 // Retrieve all conferences from the database.
-exports.findAll = (req, res) => {
-    const topic = req.query.topic;
+exports.readConferenceAll = () => {
+    const topic = query.topic;
     var condition = topic ? { topic: { $regex: new RegExp(topic), $options: "i" } } : {};
 
-    Conference.find(condition)
+    Confer.find(condition)
         .then(data => {
-            res.send(data);
+            console.log(data);
         })
         .catch(err => {
-            res.status(500).send({
+            console.log({
                 message: err.message || "Some error occurred while retrieving conferences."
             });
         });
 };
 
 // Find a single Conference with an id
-exports.findOne = (req, res) => {
-    const id = req.params.id;
+exports.readConference = (id) => {
+    //const id = params.id;
 
-    Conference.findById(id)
+    Confer.findById(id)
         .then(data => {
             if (!data)
-                res.status(404).send({ message: "Not found conference with id " + id });
-            else res.send(data);
+                console.log({ message: "Not found conference with id " + id });
+            else console.log(data);
         })
         .catch(err => {
-            res
-                .status(500)
-                .send({ message: "Error retrieving conference with id=" + id });
+            console.log
+
+                ({ message: "Error retrieving conference with id=" + id });
         });
 };
 
 // Update a Conference by the id in the request
-exports.update = (req, res) => {
-    if (!req.body) {
-        return res.status(400).send({
+exports.updateConference = (id, newConf) => {
+    newConf = typeof conferSchema
+    if (!newConf || !id) {
+        return console.log({
             message: "Data to update can not be empty!"
         });
     }
 
-    const id = req.params.id;
+    //const id = req.params.id;
 
-    Conference.findByIdAndUpdate(id, req.body, { useFindAndModify: false })
+    Confer.findByIdAndUpdate(id, newConf, { useFindAndModify: false })
         .then(data => {
             if (!data) {
-                res.status(404).send({
+                return console.log({
                     message: `Cannot update Conference with id=${id}. Maybe Conference was not found!`
                 });
-            } else res.send({ message: "Conference was updated successfully." });
+            } else console.log({ message: "Conference was updated successfully." });
         })
         .catch(err => {
-            res.status(500).send({
+            return console.log({
                 message: "Error updating Conference with id=" + id
             });
         });
 };
 
 // Delete a Conference with the specified id in the request
-exports.deleteId = (req, res) => {
-    const id = req.params.id;
+exports.deleteConference = (id) => {
+    //const id = req.params.id;
 
-    Conference.findByIdAndRemove(id, { useFindAndModify: false })
+    Confer.findByIdAndRemove(id, { useFindAndModify: false })
         .then(data => {
             if (!data) {
-                res.status(404).send({
+                return console.log({
                     message: `Cannot delete conference with id=${id}. Maybe conference was not found!`
                 });
             } else {
-                res.send({
+                return console.log({
                     message: "conference was deleted successfully!"
                 });
             }
         })
         .catch(err => {
-            res.status(500).send({
+            return console.log({
                 message: "Could not delete conference with id=" + id
             });
         });
 };
 
 // Delete all conferences from the database.
-exports.deleteAll = (req, res) => {
-    Conference.deleteMany({})
+exports.deleteConferenceAll = () => {
+    Confer.deleteMany({})
         .then(data => {
-            res.send({
+            return console.log({
                 message: `${data.deletedCount} conferences were deleted successfully!`
             });
         })
         .catch(err => {
-            res.status(500).send({
+            return console.log({
                 message: err.message || "Some error occurred while removing all conferences."
             });
         });
 };
+
 
 
 

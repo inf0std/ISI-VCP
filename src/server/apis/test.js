82e33630d733df61176996f8029c59b3a71db50e
= <=>
2022-12-03 13:01:08 +0100

    new

--- src/server/apis/test.js
+++ src/server/apis/test.js
@@ -1,29 +1,25 @@
-const {connectDb}=require('..//db/db.js');
+const { connectDb } = require('./db.js');
 const express = require('express');
 const app = express();
-const {createUser,readoneUser,UpdateLogin,UpdateUser,archiveUser,deleteUser, addContact,auth} = require('../db//crudUtils/userCrud');
-const port = process.env.PORT || 3000;//ebergement
+const { createUser, readoneUser, UpdateLogin, UpdateUser, archiveUser, deleteUser, addContact } = require('./crudUtils/userCrud');
+const port = process.env.PORT || 3000; //ebergement
 
-/*
-createUser('khelifa@gmail.coml', '00000000000000');
-createUser('saida@gmail.coml', '0055000000000');
-createUser('ahmed@gmail.coml', '0000000000FFFD0000');
-createUser('ficel@gmail.coml', '000000000055550000');*/
 
-/*
-UpdateUser('63828f04629963f37bfc932c', {pic :'img.jpeg',username:'khlifa'});
-UpdateUser('63828f04629963f37bfc9330', {pic :'profile.jpeg',username:'saida'});
-UpdateUser('63828f04629963f37bfc9332', {username:'ahmed'});
-UpdateUser('63828f04629963f37bfc932e', {pic :'09.jpeg'});
-*/
+//createUser({ email: 'fayce@gmail.coml' }, { password: 'faycel' });
+//UpdateUser('6381fb93160d73accb59d1df', { username: 'faycel' });
+//addContact('6381fb9316gpçj793accb59d1df', '6381fb93160dsçf73accb59d1df');
 
-//addContact('63828f04629963f37bfc932c','63828f04629963f37bfc9330' );
-UpdateLogin('63828f04629963f37bfc9332',{email:'hmedon@gmail.com',password:'kkkkkl'});
-//auth('khelif@gmail.coml','00000000000000')
+
+//findOne('6388e49a6f3b0b10350b6533');
+//update('6388e49a6f3b0b10350b6533', { topic: 'updated', users: '6381e972cd98f245e2c7123c' });
+//JoinedToConference();
+//LeaveTheConference();
+//deleteId('6388e49a6f3b0b10350b6533');
+//deleteAll('');
+//findAll('');
+//create('topic3', '6381e972cd98f245e2c7123c');
 /*
 app.use(express.json());
-
-
 // lancer aplication sur notre port
 app.listen(port,() => {console.log(`server lancer sur le port a:http//localhost:${port}`);
 //The app.listen() function is used to bind and listen the connections on the specified host and port.
@@ -31,5 +27,3 @@ app.listen(port,() => {console.log(`server lancer sur le port a:http//localhost:
 });
 
 */
-
-

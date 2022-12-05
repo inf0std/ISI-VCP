4ede93931ec7e8df132e513039bf19446da4bb8f
[Faycel ahcene] <ff_ahcene@esi.dz>
2022-12-05 22:07:07 +0100

    did nothing

--- src/server/modules/users.js
+++ src/server/modules/users.js
@@ -1,36 +1,32 @@
-const users =[];
+const users = [];
 
-const addUser =({id,name,room})=>{
-    name = name.tirm().toLowerCase();
-    room  = room.tirm().toLowerCase();
+const addUser = ({ id, name, room }) => {
+  name = name.tirm().toLowerCase();
+  room = room.tirm().toLowerCase();
 
+  // verifier l'existance de users dans la meme room avec le meme nom
 
- // verifier l'existance de users dans la meme room avec le meme nom
-
- const existingUser = users.find((user)=> user.room === room && user.name === name);
+  const existingUser = users.find(
+    (user) => user.room === room && user.name === name
+  );
   if (existingUser) {
-    return{ error: 'Username is taken'}; 
+    return { error: "Username is taken" };
   }
 
   //si non cree un nv user
   const user = { id, name, room };
   users.push(user);
-  return{user}
+  return { user };
+};
+const removeUser = (id) => {
+  const index = users.findIndex((user) => user.id === id);
+  if (index !== -1) {
+    return users.splice(index, 1)[0];
+  }
+};
 
-}
-const removeUser =(id)=> {
-    const index =users.findIndex((user)=> user.id ===id);
-    if (index !==-1){
-        return users.splice(index,1)[0];
-    }
+const getUser = (id) => users.find((user) => user.id === id);
 
-}
+const getUsersInRoom = (room) => users.filter((user) => user.room === room);
 
-const getUser = (id) => users.find((user )=>user.id ===id);
-
-
-
-const getUsersInRoom =(room)=> users.filter((user)=>user.room === room);
-
-
-module.exports ={addUser,removeUser,getUser,getUsersInRoom};
+module.exports = { addUser, removeUser, getUser, getUsersInRoom };

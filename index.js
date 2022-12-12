376b93cf078889c63bfbd2fa9e9fb7a645553ac3
[Faycel ahcene] <ff_ahcene@esi.dz>
2022-12-12 22:14:24 +0100

    bug fix

--- index.js
+++ index.js
@@ -1,44 +1,33 @@
-
-const express = require('express');
-const socket = require('socket.io');
-const http = require('http');
+const express = require("express");
+const Server = require("socket.io");
+const http = require("http");
 
 const app = express();
 const server = http.createServer(app);
-const io = socket(server,{
-    cors:{
-        origin :"*",//any user
-        methods :["GET","Post"]
-    }
-})
-app.use(express.static(__dirname+'/client'))
-app.get("/",(req,res)=>{
-    res.status(200);
-    res.send('connected');
-})
+const io = Server(server);
+app.use(express.static(__dirname + "/public"));
+app.get("/", (req, res) => {
+  res.status(200);
+  res.send("connected");
+});
+
+io.on("connection", (socket) => {
+  console.log("connected to", socket.id);
+  socket.join("room1");
+  console.log("rooms", socket.rooms);
+  io.to("room1").emit("msg", {
+    room: "room1",
+    msg: "joined room1",
+  });
+
+  socket.on("msg", (data) => {
+    console.log(socket.rooms);
+    io.to(data.room).emit("msg", data);
+    console.log("msg send", data.room, data);
+  });
+});
+
 let PORT = 4000;
 server.listen(PORT, () => {
-    console.log("Listening in the port",PORT);
-  })
-  const room =[];
-
-  const nvroom =(nom)=>{
-    io.of(nom);
-    room.push(nvroom);
-  }
- /*nvroom ('room1');
- nvroom('room2');*/
-
-  
-io.on("connection", (socket) => {
-    console.log("connected to", socket.id) 
-    socket.join(["room1","room2"]);   
-    io.to('room1').emit('msg', {
-        room :"room1",
-        msg :"joined room1"
-    })
-    socket.on("msg", (data) => {
-        io.to(data.room).emit("msg", data)
-        console.log("msg send");
-    })
-})
+  console.log("Listening in the port", PORT);
+});
--- public/index.js
+++ public/index.js
@@ -1,37 +1,32 @@
-
-const socket = io("http://localhost:4000");
+const socket = io();
 //const messageform = document.querySelector(".chatbox form");
 //const messageList = document.querySelector("#messagelist");
 const userList = document.querySelector("ul#users");
 
-const  messages = []
+const messages = [];
 //socket listeners
 //socket.join(['room1','room2']) ;
+const envoimessage = (e) => {
+  e.preventDefault();
+  var chatboxinput = document.getElementById("chatboxinput-" + e.target.id);
+  socket.emit("msg", {
+    room: e.target.id,
+    msg: chatboxinput.value,
+  });
+  console.log("send message", e.target.id, chatboxinput.value);
+  chatboxinput.value = "";
+};
 
-const envoimessage = (e)=>{
- var chatboxinput=document.getElementById("chatboxinput-"+e.target.id);
- socket.emit("msg",{
-    room:e.target.id,
-    msg:chatboxinput.value
- })
- console.log('send message')
- e.preventDefault()
-}
-socket.on("msg",(data)=>
-{
-    room = data.room;
-    messageList = document.getElementById("messageList-" +room);
-    console.log("room", room);
-    console.log(messageList)
-    messageList.innerHTML +=`<li>
+socket.on("msg", (data) => {
+  room = data.room;
+  messageList = document.getElementById("messageList-" + room);
+  console.log("room", room);
+  console.log(messageList);
+  messageList.innerHTML += `<li>
     <p>${data.msg}</p>
 
-      </li>`
+      </li>`;
 });
-socket.on("message_client", (message) => {
-    messages.push(message);
-    updateMessages()
-})
 //event listeners
 /*messageform.addEventListener('submit', messageSubmitHandler)
 function messageSubmitHandler(e) {
@@ -48,20 +43,15 @@ function messageSubmitHandler(e) {
 
 }*/
 
-
 function updateMessages() {
-    messageList.textContent = ''
-    for (let i = 0; i < messages.length; i++) {
-        messageList.innerHTML += `<li>
+  messageList.textContent = "";
+  for (let i = 0; i < messages.length; i++) {
+    messageList.innerHTML += `<li>
                      <p>${messages[i].message}</p>
         
-                       </li>`
-    }
+                       </li>`;
+  }
 }
- 
-document.getElementById("room1").onclick = envoimessage; 
+
+document.getElementById("room1").onclick = envoimessage;
 document.getElementById("room2").onclick = envoimessage;
-
-
-
-

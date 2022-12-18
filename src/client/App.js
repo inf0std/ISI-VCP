1bd4fec0ded8e57af42867c2d80653d97f8de5c5
ania <ania.rahmani@fgei.ummto.dz>
2022-12-18 14:04:55 +0100

    added files

--- src/client/App.js
+++ src/client/App.js
@@ -1,5 +1,26 @@
+/*import './App.css';
+import Sidebar from './component/userProfile/Sidebar';
+import Feed from './component/userProfile/Feed';
+import Head from './component/userProfile/Head';
 
-import React from 'react';
+
+function App(){
+  return(
+
+    <div className='app' >
+       <Head/>
+       <div className='app_body'>
+        <Sidebar/>
+        <Feed/>
+       </div>
+   </div>
+
+  )
+
+}
+export default App;*/
+
+/*import React from 'react';*/
 
 /*import PrgmConference from'./component/Programmation/PrgmConference'
 import PrgmDebat from'./component/Programmation/PrgmDebat'

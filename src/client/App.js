3b99384bc42dfaac535bebb8c9f7f36b553f2f3b
[Faycel ahcene] <ff_ahcene@esi.dz>
2022-12-18 17:21:45 +0100

    resolved merge conflict

--- src/client/App.js
+++ src/client/App.js
@@ -1,23 +1,9 @@
-<<<<<<< HEAD
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <openConversation/>
-      <videoCollection/>
-    </div>
-  );
-}
-=======
-
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./views/Home";
 import Contact from "./views/Contact";
 import Chat from "./views/Chat";
 import SignInSignUp from "./component/formulaire/modalForms/SignINSignUp";
-import VideoRoom from './views/VideoRoom'
 const App = (props) => {
   return (
     <BrowserRouter>
@@ -26,11 +12,9 @@ const App = (props) => {
         <Route path="/Contact" element={<Contact />} />
         <Route path="/Chat" element={<Chat />} />
         <Route path="/login" element={<SignInSignUp />} />
-        <Route path="/VideoRoomUI" element={< VideoRoom/>} />
       </Routes>
     </BrowserRouter>
   );
 };
->>>>>>> origin/ui
 
 export default App;
--- src/client/index.js
+++ src/client/index.js
@@ -1,15 +1,3 @@
-<<<<<<< HEAD
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-ReactDOM.render(
- <React.StrictMode>
- <App />
- </React.StrictMode>,
- document.getElementById('root')
-=======
 import React from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
@@ -22,13 +10,8 @@ root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
->>>>>>> origin/ui
 );
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-<<<<<<< HEAD
 reportWebVitals();
-=======
-reportWebVitals();
->>>>>>> origin/ui

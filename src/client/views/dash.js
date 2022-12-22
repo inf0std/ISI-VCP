268f3230384fd6782c457063597510114dcb1006
= <=>
2022-12-22 17:21:19 +0100

    dash

---
+++ src/client/views/dash.js
@@ -0,0 +1,18 @@
+import "./dash.css";
+import MainDash from "../component/dash/MainDash/MainDash";
+import RightSide from "../component/dash/RigtSide/RightSide";
+import Sidebar from "../component/dash/Sidebar";
+
+function dashbord() {
+  return (
+    <div className="App">
+      <div className="AppGlass">
+        <Sidebar />
+        <MainDash />
+        <RightSide />
+      </div>
+    </div>
+  );
+}
+
+export default dashbord;

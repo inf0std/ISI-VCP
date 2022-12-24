abba9fab5005477dfe549b94c2ff597b8c454434
= <=>
2022-12-24 03:41:35 +0100

    new

--- src/server/apis/router.js
+++ src/server/apis/router.js
@@ -1,11 +1,6 @@
 var express = require("express");
 const { User } = require("../db/schema/User");
-const {
-  createUser,
-  readoneUser,
-  auth,
-  readcontacts,
-} = require("../db/crudUtils/userCrud");
+
 const mongoose = require("mongoose");
 const router = express.Router();
 const { loginrequired } = require("../db/crudUtils/config/JWT");
@@ -58,49 +53,20 @@ router.get("/", function (req, res) {
 router.post("/signin", verifiedemail, handleLogin);
 //signUp
 router.post("/signup", registerUser);
-
 router.get("/ver", handlevalidateemail);
-
 router.get("/users/:id/contacts", handleUserContacts);
-//////////////////////////
 router.get("/users/:id/conversations", handleUserConversations);
-
-/////////////////
 router.get("/users/:id/conferences", handleuserconference);
-///////////
-/////////////////
 router.get("/users/:id/reunions", handleuserreunion);
-///////////organisations
-/////////////////
 router.get("/users/:id/organisations", handleuserorganisations);
-
-/////////////////
 router.get("/conversation/:id/", handleconversation);
-/////////////////
 router.get("/conversation/:id/messages", handleconvesationmsg);
 /////////////////
-
 router.post("/user/:id/update", handleUpdateUser);
 router.post("/user/:id/updatepasse", handleupdatepasse);
 router.post("/user/:id/updateemail", handleupdatemail);
-router.post("/subscribe", function (req, res) {
-  //profile
-  res.send("");
-});
 router.post("/user/:id1/deletecontact/:id2", deletecontact);
-
-router.get("/user/notification", function (req, res) {
-  //profile
-  res.send("");
-});
 router.post("/user/:id1/contact/:id2", addContact);
-router.get("/conversation", function (req, res) {
-  //profile
-  res.send("");
-});
-
-router.post("/user/program");
-router.get;
 
 //Conferences
 router.post("/users/:idU/createConference", handleCreateConference);
@@ -115,6 +81,18 @@ router.post("/reunions/readReunionAll", handleReunion);
 router.post("/reunions/:id/updateReunion", handleUpdateReunion);
 router.post("/reunions/:id/deleteReunion", handleDeleteReunion);
 router.post("/reunions/deleteReunionAll", handleDeleteReunionAll);
+
+router.get("/user/notification", function (req, res) {
+  //profile
+  res.send("");
+});
+
+router.get("/conversation", function (req, res) {
+  //profile
+  res.send("");
+});
+
+router.post("/user/program");
 
 module.exports = router;
 

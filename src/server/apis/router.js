235aa898b16d6ad6e74b7147761b304efd5de5e3
= <=>
2022-12-24 03:10:47 +0100

    new

--- src/server/apis/router.js
+++ src/server/apis/router.js
@@ -1,49 +1,63 @@
 var express = require("express");
 const { User } = require("../db/schema/User");
 const {
-    createUser,
-    readoneUser,
-    auth,
-    readcontacts,
+  createUser,
+  readoneUser,
+  auth,
+  readcontacts,
 } = require("../db/crudUtils/userCrud");
 const mongoose = require("mongoose");
 const router = express.Router();
 const { loginrequired } = require("../db/crudUtils/config/JWT");
 const { verifiedemail } = require("../db/crudUtils/config/JWT");
 const { deletecontact, addContact } = require("./routeUtils");
-const { handleCreateConference, handleConference, handleUpdateConference, handleDeleteConference, handleDeleteConferenceAll } = require("./ConferenceRouteHandlers");
 const {
-    handleupdatemail,
-    handleupdatepasse,
-    handleLogin,
-    handleSignUp,
-    handleUserConversations,
-    handleUserContacts,
-    handleconvesationmsg,
-    handleconversation,
-    handleuserreunion,
-    handleuserconference,
-    handleuserorganisations,
-    handlevalidateemail,
-    handleUpdateUser,
+  handleCreateConference,
+  handleConference,
+  handleUpdateConference,
+  handleDeleteConference,
+  handleDeleteConferenceAll,
+} = require("./ConferenceRouteHandlers");
+const {
+  handleupdatemail,
+  handleupdatepasse,
+  handleLogin,
+
+  handleUserConversations,
+  handleUserContacts,
+  handleconvesationmsg,
+  handleconversation,
+  handleuserreunion,
+  handleuserconference,
+  handleuserorganisations,
+  handlevalidateemail,
+  handleUpdateUser,
 } = require("./userRouteHandlers");
-const { handleCreateReunion, handleReunion, handleUpdateReunion, handleDeleteReunion, handleDeleteReunionAll } = require("./ReunionRouteHandlers");
+
+const { registerUser, allUsers } = require("./signinsignup");
+const {
+  handleCreateReunion,
+  handleReunion,
+  handleUpdateReunion,
+  handleDeleteReunion,
+  handleDeleteReunionAll,
+} = require("./ReunionRouteHandlers");
 
 // middleware that is specific to this router
 router.use(function timeLog(req, res, next) {
-    console.log("Time: ", Date.now());
-    next();
+  console.log("Time: ", Date.now());
+  next();
 });
 // define the home page route
-router.get("/", function(req, res) {
-    //profile
-    res.send("");
+router.get("/", function (req, res) {
+  //profile
+  res.send("");
 });
 //////
 //signIn
 router.post("/signin", verifiedemail, handleLogin);
 //signUp
-router.post("/signup", handleSignUp);
+router.post("/signup", registerUser);
 
 router.get("/ver", handlevalidateemail);
 
@@ -69,27 +83,24 @@ router.get("/conversation/:id/messages", handleconvesationmsg);
 router.post("/user/:id/update", handleUpdateUser);
 router.post("/user/:id/updatepasse", handleupdatepasse);
 router.post("/user/:id/updateemail", handleupdatemail);
-router.post("/subscribe", function(req, res) {
-    //profile
-    res.send("");
+router.post("/subscribe", function (req, res) {
+  //profile
+  res.send("");
 });
 router.post("/user/:id1/deletecontact/:id2", deletecontact);
 
-router.get("/user/notification", function(req, res) {
-    //profile
-    res.send("");
+router.get("/user/notification", function (req, res) {
+  //profile
+  res.send("");
 });
 router.post("/user/:id1/contact/:id2", addContact);
-router.get("/conversation", function(req, res) {
-    //profile
-    res.send("");
+router.get("/conversation", function (req, res) {
+  //profile
+  res.send("");
 });
 
 router.post("/user/program");
 router.get;
-
-
-
 
 //Conferences
 router.post("/users/:idU/createConference", handleCreateConference);
@@ -104,9 +115,6 @@ router.post("/reunions/readReunionAll", handleReunion);
 router.post("/reunions/:id/updateReunion", handleUpdateReunion);
 router.post("/reunions/:id/deleteReunion", handleDeleteReunion);
 router.post("/reunions/deleteReunionAll", handleDeleteReunionAll);
-
-
-
 
 module.exports = router;
 
---
+++ src/server/apis/signinsignup.js
@@ -0,0 +1,97 @@
+const { default: mongoose } = require("mongoose");
+const validator = require("validator");
+const bcrypt = require("bcryptjs");
+const createError = require("http-errors");
+const { User } = require("../db/schema/User");
+const Conversation = require("../db/schema/Conversation");
+const jwt = require("jsonwebtoken");
+const cookie = require("cookie-parser");
+const nodemailer = require("nodemailer");
+const crypto = require("crypto");
+const { count } = require("console");
+const asyncHandler = require("express-async-handler");
+
+const allUsers = asyncHandler(async (req, res) => {
+  const keyword = req.query.search
+    ? {
+        $or: [
+          { name: { $regex: req.query.search, $options: "i" } },
+          { login: { email: { $regex: req.query.search, $options: "i" } } },
+        ],
+      }
+    : {};
+
+  const users = await User.find(keyword).find({ _id: { $ne: req.user._id } });
+  res.send(users);
+});
+
+//email
+var transporter = nodemailer.createTransport({
+  service: "Gmail",
+  auth: {
+    user: "seen.project.cpi@gmail.com",
+    pass: "dehbaarxnwdujndl",
+  },
+  tls: {
+    rejectUnauthorized: false,
+  },
+});
+
+const registerUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    res.status(400);
+    throw new Error("Please Enter all the Feilds");
+  }
+
+  const userExists = await User.findOne({ login: { email } });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error("User already exists");
+  }
+
+  const user = await User.create({
+    login: {
+      email,
+      password,
+    },
+    isadmin: false,
+    isverified: false,
+    emailtoken: crypto.randomBytes(64).toString("hex"),
+  });
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.username,
+      email: user.login.email,
+      isAdmin: user.isAdmin,
+      pic: user.pic,
+      token: user.emailtoken,
+    });
+
+    //send email verification
+    var mailOptions = {
+      from: '"Verify your email"<mira98315@gmail.com>',
+      to: user.login.email,
+      subject: `${user.username}  verify your email`,
+      html: `<h2> ${user.username}! Thanks for registring on our site </h2>
+  <h4>Please verify your email to continue... </h4>
+  <a href = "http://127.0.0.1:8080/api/ver?token=${user.emailtoken}">verify your email</a>`,
+    };
+    //send email
+    transporter.sendMail(mailOptions, function (error, info) {
+      if (error) {
+        console.log(error);
+      } else {
+        console.log("verification email is sent to your gmail account");
+      }
+    });
+  } else {
+    res.status(400);
+    throw new Error("User not found");
+  }
+});
+module.exports = { registerUser, allUsers };

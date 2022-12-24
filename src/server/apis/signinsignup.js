0646494d21618931148c2349eb08309069dca251
amira <amira.sellah@fgei.ummto.dz>
2022-12-24 19:31:50 +0100

    Update signinsignup.js

--- src/server/apis/signinsignup.js
+++ src/server/apis/signinsignup.js
@@ -11,87 +11,87 @@ const crypto = require("crypto");
 const { count } = require("console");
 const asyncHandler = require("express-async-handler");
 
-const allUsers = asyncHandler(async (req, res) => {
-  const keyword = req.query.search
-    ? {
-        $or: [
-          { name: { $regex: req.query.search, $options: "i" } },
-          { login: { email: { $regex: req.query.search, $options: "i" } } },
-        ],
-      }
-    : {};
+const allUsers = asyncHandler(async(req, res) => {
+    const keyword = req.query.search ?
+        {
+            $or: [
+                { name: { $regex: req.query.search, $options: "i" } },
+                { login: { email: { $regex: req.query.search, $options: "i" } } },
+            ],
+        } :
+        {};
 
-  const users = await User.find(keyword).find({ _id: { $ne: req.user._id } });
-  res.send(users);
+    const users = await User.find(keyword).find({ _id: { $ne: req.user._id } });
+    res.send(users);
 });
 
 //email
 var transporter = nodemailer.createTransport({
-  service: "Gmail",
-  auth: {
-    user: "seen.project.cpi@gmail.com",
-    pass: "dehbaarxnwdujndl",
-  },
-  tls: {
-    rejectUnauthorized: false,
-  },
+    service: "Gmail",
+    auth: {
+        user: "seen.project.cpi@gmail.com",
+        pass: "dehbaarxnwdujndl",
+    },
+    tls: {
+        rejectUnauthorized: false,
+    },
 });
 
-const registerUser = asyncHandler(async (req, res) => {
-  const { email, password } = req.body;
+const registerUser = asyncHandler(async(req, res) => {
+    const { email, password } = req.body;
 
-  if (!email || !password) {
-    res.status(400);
-    throw new Error("Please Enter all the Feilds");
-  }
+    if (!email || !password) {
+        res.status(400);
+        throw new Error("Please Enter all the Feilds");
+    }
 
-  const userExists = await User.findOne({ login: { email } });
+    const userExists = await User.findOne({ login: { email } });
 
-  if (userExists) {
-    res.status(400);
-    throw new Error("User already exists");
-  }
+    if (userExists) {
+        res.status(400);
+        throw new Error("User already exists");
+    }
 
-  const user = await User.create({
-    login: {
-      email,
-      password,
-    },
-    isadmin: false,
-    isverified: false,
-    emailtoken: crypto.randomBytes(64).toString("hex"),
-  });
-
-  if (user) {
-    res.status(201).json({
-      _id: user._id,
-      name: user.username,
-      email: user.login.email,
-      isAdmin: user.isAdmin,
-      pic: user.pic,
-      token: user.emailtoken,
+    const user = await User.create({
+        login: {
+            email,
+            password,
+        },
+        isadmin: false,
+        isverified: false,
+        emailtoken: crypto.randomBytes(64).toString("hex"),
     });
 
-    //send email verification
-    var mailOptions = {
-      from: '"Verify your email"<mira98315@gmail.com>',
-      to: user.login.email,
-      subject: `${user.username}  verify your email`,
-      html: `<h2> ${user.username}! Thanks for registring on our site </h2>
+    if (user) {
+        res.status(201).json({
+            _id: user._id,
+            name: user.username,
+            email: user.login.email,
+            isAdmin: user.isAdmin,
+            pic: user.pic,
+            token: user.emailtoken,
+        });
+
+        //send email verification
+        var mailOptions = {
+            from: `Verify your email<${user.login.email}>`,
+            to: user.login.email,
+            subject: `${user.username}  verify your email`,
+            html: `<h2> ${user.username}! Thanks for registring on our site </h2>
   <h4>Please verify your email to continue... </h4>
   <a href = "http://127.0.0.1:8080/api/ver?token=${user.emailtoken}">verify your email</a>`,
-    };
-    //send email
-    transporter.sendMail(mailOptions, function (error, info) {
-      if (error) {
-        console.log(error);
-      } else {
-        console.log("verification email is sent to your gmail account");
-      }
-    });
-  } else {
-    res.status(400);
-    throw new Error("User not found");
-  }
+        };
+        //send email
+        transporter.sendMail(mailOptions, function(error, info) {
+            if (error) {
+                console.log(error);
+            } else {
+                console.log("verification email is sent to your gmail account");
+            }
+        });
+    } else {
+        res.status(400);
+        throw new Error("User not found");
+    }
 });
 module.exports = { registerUser, allUsers };

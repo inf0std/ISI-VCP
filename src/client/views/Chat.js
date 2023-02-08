18b98bb7dac8f2f2e451645033f031a2664dcedc
imanebennabi <imane.bennabi@fgei.ummto.dz>
2023-02-08 13:38:07 +0100

    a

--- src/client/views/Chat.js
+++ src/client/views/Chat.js
@@ -1,23 +1,40 @@
-import { useState, useEffect } from "react";
-// import NavBar from '../component/navbar/NavBar'
-import Conversations from "../component/conversations/conversations";
-import ConversationTab from "../component/conversationTab/conversationTab";
+import React, { useEffect, useRef, useState } from "react";
 
-import ChatHeader from "./Chat/ChatHeader";
-import ChatZone from "./Chat/ChatZone";
-import ChatList from "./Chat/ChatList";
-const Chat = ({ generalHandler, localVars }) => {
-  const [activeConv, setActiveConv] = useState(0);
-  const [msgs, setMsgs] = useState([]);
-  useEffect(() => {});
-  const sendMessage = () => {};
+const ChatUI = ({generalHandler, localVars}) => {
+  const msgRef = useRef()
+  const [messages, setMessages] = useState([]);
+  const [convId, setConvId] = useState(1)
+
+  useEffect(()=>{
+    localVars.socket.on('msg', ({cid, content})=>{
+      setMessages([...messages, content])
+    })
+  })
+  const handleSubmit = event => {
+    event.preventDefault();
+    setMessages([...messages, message]);
+    setMessage("");
+    localVars.socket.emit('msg', {cid: convId,content: msgRef.current.value})
+    msgRef.current.value=''
+  };
+
   return (
-<div>
-      <ChatHeader />
-      <ChatList />
-      <ChatZone />
-</div>
+    <div>
+      <ul>
+        {messages.map((m, index) => (
+          <li key={index}>{m}</li>
+        ))}
+      </ul>
+      <form onSubmit={handleSubmit}>
+        <input
+          ref={msgRef}
+          type="text"
+          onChange={e => setMessage(e.target.value)}
+        />
+        <button type="submit" onClick={handleSubmit}>Send</button>
+      </form>
+    </div>
   );
 };
 
-export default Chat;
+export default ChatUI;
